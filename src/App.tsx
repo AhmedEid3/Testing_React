b8996549eb4e87d841da14272364a823e41d78eb
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-07 08:58:05 +0300

    Update App Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Box } from '@mui/material';
 import Counter from './components/Counter/Counter';
+import MuiMode from './components/MuiMode/MuiMode';
 
 function App() {
-  return <Counter />;
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <Counter />
+      <MuiMode />
+    </Box>
+  );
 }
 
 export default App;
